0667c8538e0a23320c84e1b49fce962e2dc222de
Heidi <heidiks@gmail.com>
2016-04-09 14:10:56 -0300

    Limit and gitignore

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
 	def index
-		@productsByName = Product.order :name
-		@productsByPrice = Product.order :price
+		@productsByName = Product.order(:name).limit 5
+		@productsByPrice = Product.order(:price).limit 2
 	end
 end
