fb0bc5a0178e41fc0c361bb88a41d72bc33e117c
Heidi <heidiks@gmail.com>
2016-04-10 19:52:04 -0300

    Update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,14 @@
 class ProductsController < ApplicationController
 
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @productsByName = Product.order(:name).limit 5
     @productsByPrice = Product.order(:price).limit 2
   end
 
   def create
-    valores = params.require(:product).permit(:name, :description, :quantity, :price, :department_id)
-    @product = Product.new valores
+    @product = Product.new product_params
     if @product.save
       flash[:notice] = "Produto salvo com sucesso"
       redirect_to root_url
@@ -18,18 +19,46 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @departments = Department.all
+    render_new
   end
 
   def destroy
-    id = params[:id]
-    Product.destroy id
+    @product.destroy
     redirect_to root_url
   end
 
   def search
     @name_to_search = params[:nome]
     @products = Product.where "name like ?", "%#{@name_to_search}%"
+  end
+
+  def edit
+    render_new
+  end
+
+  def update
+    if @product.update product_params
+      flash[:notice] = "Produto atualizado com sucesso"
+      redirect_to root_url
+    else
+      render_new
+    end
+  end
+
+  private
+
+  def set_product
+    id = params[:id]
+    @product = Product.find(id)
+  end
+
+  def render_new
+    @departments = Department.all
+    render :new
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :quantity, :price, :department_id)
   end
 
 
